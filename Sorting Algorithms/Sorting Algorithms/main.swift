5839366f291cbd004034f91cc708621620c30ba9
Andrei Blaj <andreiblajdev@gmail.com>
2020-02-25 21:21:54 -0500

    merge sort

---
+++ Sorting Algorithms/Sorting Algorithms/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Sorting Algorithms
+//
+//  Created by Andrei Blaj on 2/25/20.
+//  Copyright © 2020 Andrei Blaj. All rights reserved.
+//
+
+import Foundation
+
+let example: [Int] = [7, 1, 5, 3, 9, 2, 6, 4, 8]
+
+print(mergeSort(example))
+
+
