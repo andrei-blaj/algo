453bee37228dcfb1dfc22ac7efbfd3e0509d255c
Andrei Blaj <andreiblajdev@gmail.com>
2020-02-28 22:52:01 -0500

    ssm

--- Random.playground/Contents.swift
+++ Random.playground/Contents.swift
@@ -115,3 +115,82 @@ func expandFromMiddle(_ s: [Character], left: Int, right: Int) -> Int {
 longestPalindrome("racecar")
 
 
+// Given an array of N numbers (positive or negative integers) and K, calculate the subsequence, of length at least K, that has the largest sum.
+
+// i, j and sum
+
+let v = [2, -3, 5, 1, -4, 4, 12, -14, -5, 22, 32]
+// k = 2
+
+// brute force
+func subsequence(_ a: [Int], k: Int) -> (Int, Int, Int) {
+    
+    var s: [Int] = Array(repeating: 0, count: a.count)
+    s[0] = a[0]
+    
+    for i in 1..<a.count {
+        s[i] = a[i] + s[i-1]
+    }
+    
+    var soli = 0
+    var solj = 0
+    var sol = -1000000000
+    
+    for i in k...a.count {
+        for j in 0...(a.count - i) {
+            
+            let upperBound = s[j + i - 1]
+            let lowerBound = j == 0 ? 0 : s[j - 1]
+            
+            if upperBound - lowerBound > sol {
+                sol = upperBound - lowerBound
+                soli = j
+                solj = j + i - 1
+            }
+        }
+    }
+    
+    return (soli, solj, sol)
+}
+
+subsequence(v, k: 2)
+
+// optimized
+// ssm
+func maximumSumSubsequence(_ a: [Int]) -> Int {
+    
+    // s[i] - the maximum sum of a subsequence from (0...i) of any length
+    var s: [Int] = Array(repeating: 0, count: a.count)
+    s[0] = a[0]
+    
+    var sol = -1000000000
+    
+    for i in 1..<a.count {
+        s[i] = max(a[i], s[i - 1] + a[i])
+        sol = max(sol, s[i])
+    }
+    
+    return sol
+    
+}
+
+func maximumSumSubsequence(_ a: [Int], k: Int) -> Int {
+    
+    // s[i] - the maximum sum of a subsequence from (0...i) of length at least k
+    var s: [Int] = Array(repeating: 0, count: a.count)
+    s[0] = a[0]
+    
+    var sol = -1000000000
+    
+    for i in 1..<a.count {
+        
+    }
+    
+    return sol
+    
+}
+maximumSumSubsequence(v)
+
+
+
+
