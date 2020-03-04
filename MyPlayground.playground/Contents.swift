45d8974b9423aa0ba9a9325267c7cb71cf6c5b72
Andrei Blaj <andreiblajdev@gmail.com>
2020-03-03 21:57:25 -0500

    new problem

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -124,42 +124,100 @@ maxAreaOfIsland([[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]])
 func openLock(_ deadends: [String], _ target: String) -> Int {
     
     var visited: [String] = []
-    var queue: [(String, Int)] = []
-    
-    visited.append("0000")
-    queue.append(("0000", 0))
-    
-    while !queue.isEmpty {
-        
-        let front = queue.removeFirst()
-        let currentCombination = front.0
-        let currentIndex = front.1
-        
-        print(currentCombination, currentIndex )
-        
-        if deadends.contains(currentCombination) { continue }
-        
-        if currentCombination == target {
-            return currentIndex
-        }
+    var queue: [String] = []
 
-        var comb = Array(currentCombination)
-        
-        for i in 0..<4 {
-            for j in 0...9 {
-                comb[i] = Character(String(j))
-                let strComb = String(comb)
-                if !visited.contains(strComb) && !deadends.contains(strComb) {
-                    queue.append((strComb, currentIndex + 1))
-                    visited.append(strComb)
+    visited.append("0000")
+    queue.append("0000")
+    var level = 0
+
+    while !queue.isEmpty {
+        var size = queue.count
+        while size > 0 {
+            let currentCombination = queue.removeFirst()
+            
+            if deadends.contains(currentCombination) {
+                size -= 1
+                continue
+            }
+
+            if currentCombination == target { return level }
+
+            var comb1 = Array(currentCombination)
+            var comb2 = comb1
+            for i in 0..<4 {
+                comb1 = Array(currentCombination)
+                comb2 = comb1
+                if let digit = Int(String(comb1[i])) {
+                    let digit1 = (digit + 1) % 10
+                    comb1[i] = Character(String(digit1))
+
+                    var digit2 = digit - 1
+                    if digit2 < 0 {
+                        digit2 = 10 + digit2
+                    }
+                    comb2[i] = Character(String(digit2))
+
+                    let strComb1 = String(comb1)
+                    let strComb2 = String(comb2)
+
+                    if !visited.contains(strComb1) && !deadends.contains(strComb1) {
+                        queue.append(strComb1)
+                        visited.append(strComb1)
+                    }
+
+                    if !visited.contains(strComb2) && !deadends.contains(strComb2) {
+                        queue.append(strComb2)
+                        visited.append(strComb2)
+                    }
+
                 }
+            }
+            size -= 1
+        }
+        level += 1
+    }
+
+    return -1
+    
+}
+
+//openLock(["8887","8889","8878","8898","8788","8988","7888","9888"], "8888")
+
+func subdomainVisits(_ cpdomains: [String]) -> [String] {
+    
+    var sol: [String] = []
+    var dict: [String: Int] = [:]
+    
+    for cpdomain in cpdomains {
+    
+        let componenets = cpdomain.split(separator: " ")
+        let cnt = Int(componenets[0])!
+        
+        let domainComponents = componenets[1].split(separator: ".")
+        var cmpStr = ""
+        for cmp in domainComponents.reversed() {
+            
+            if cmpStr == "" {
+                cmpStr += String(cmp)
+            } else {
+                cmpStr = String(cmp) + "." + cmpStr
+            }
+            
+            if dict[cmpStr] == nil {
+                dict[cmpStr] = cnt
+            } else {
+                dict[cmpStr]! += cnt
             }
         }
         
     }
     
-    return -1
+    for (key, value) in dict {
+        sol.append(String(value) + " " + key)
+    }
+    
+    return sol
     
 }
 
-openLock(["0201","0101","0102","1212","2002"], "0202")
+print(subdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]))
