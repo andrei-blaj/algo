a9b3259253595b179c6b558234961462c6aa008e
Andrei Blaj <andreiblajdev@gmail.com>
2020-03-05 20:29:04 -0500

    more

--- gradina3.playground/Contents.swift
+++ gradina3.playground/Contents.swift
@@ -1,31 +1,24 @@
-let n = 12
-let p = 10
-let k = 5
-let pomi: [(Int, Int)] = [(4, 3), (5, 5), (6, 8), (7, 3), (7, 7), (8, 8), (9, 3), (9, 6), (10, 10), (11, 5)]
+let N = 12
+let T = 10
+let K = 5
+let trees: [(Int, Int)] = [(4, 3), (5, 5), (6, 8), (7, 3), (7, 7), (8, 8), (9, 3), (9, 6), (10, 10), (11, 5)]
 
-var A = Array(repeating: Array(repeating: 0, count: n), count: n)
+var A = Array(repeating: Array(repeating: 0, count: N), count: N)
 var S = A
 
 var solMax = -1
 var solCount = 0
 
-for pomIndex in 0..<p {
-    let i = pomi[pomIndex].0
-    let j = pomi[pomIndex].1
+for treeIndex in 0..<T {
+    let i = trees[treeIndex].0
+    let j = trees[treeIndex].1
     
-    A[i-1][j-1] = 1
-    S[i-1][j-1] = 1
+    A[i - 1][j - 1] = 1
+    S[i - 1][j - 1] = 1
 }
 
-//for i in 0..<n {
-//    for j in 0..<n {
-//        print(A[i][j], terminator: " ")
-//    }
-//    print("")
-//}
-
-for i in 0..<n {
-    for j in 0..<n {
+for i in 0..<N {
+    for j in 0..<N {
         if i > 0 && j > 0 {
             S[i][j] += S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1]
         } else if i > 0 {
@@ -36,18 +29,9 @@ for i in 0..<n {
     }
 }
 
-//print("\n")
-//
-//for i in 0..<n {
-//    for j in 0..<n {
-//        print(S[i][j], terminator: " ")
-//    }
-//    print("")
-//}
-
-for i in k..<n {
-    for j in k..<n {
-        let val = S[i][j] - S[i - k][j] - S[i][j - k] + S[i - k][j - k]
+for i in K..<N {
+    for j in K..<N {
+        let val = S[i][j] - S[i - K][j] - S[i][j - K] + S[i - K][j - K]
         if val > solMax {
             solMax = val
             solCount = 1
