616b5e5bf7f505672fe32035e2b303c498405b8a
Andrei Blaj <andreiblajdev@gmail.com>
2020-03-13 12:18:32 -0400

    combinations

--- backtracking.playground/Contents.swift
+++ backtracking.playground/Contents.swift
@@ -14,6 +14,7 @@ func backtrack<T: Comparable>(_ arr: [T], _ n: Int, _ k: Int, _ indexArray: inou
         for j in 0..<k {
             if indexArray[j] == indexArray[k] {
                 uniqueIndexes = false
+                break
             }
         }
         
@@ -31,6 +32,40 @@ func backtrack<T: Comparable>(_ arr: [T], _ n: Int, _ k: Int, _ indexArray: inou
     }
 }
 
-generatePermutations([1, 2, 3])
+//generatePermutations([1, 2, 3])
 
+func generateCombinations<T: Comparable>(_ arr: [T], _ m: Int) {
+    let n = arr.count
+    var indexArray = Array(repeating: 0, count: m)
+    backtrack(arr, n, m, 0, &indexArray)
+}
 
+func backtrack<T: Comparable>(_ arr: [T], _ n: Int, _ m: Int, _ k: Int, _ indexArray: inout [Int]) {
+    
+    for i in 0..<n {
+        indexArray[k] = i
+        
+        var uniqueIndexes = true
+        for j in 0..<k {
+            if indexArray[j] == indexArray[k] || indexArray[j] > indexArray[k] {
+                uniqueIndexes = false
+                break
+            }
+        }
+        
+        if uniqueIndexes {
+            if k == m - 1 {
+                for j in 0..<m {
+                    print(arr[indexArray[j]], terminator: " ")
+                }
+                print("")
+            } else {
+                backtrack(arr, n, m, k + 1, &indexArray)
+            }
+        }
+        
+    }
+    
+}
+
+generateCombinations([1,2,3,4], 3)
